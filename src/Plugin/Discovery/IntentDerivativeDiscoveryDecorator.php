d5679ec4f6584069327d54a4ace0e1e7f5c19cdf
Gabriele Maira <gabriele.maira@manifesto.co.uk>
2017-06-16 12:09:27 +0100

    Adding docblocks and comments

--- src/Plugin/Discovery/IntentDerivativeDiscoveryDecorator.php
+++ src/Plugin/Discovery/IntentDerivativeDiscoveryDecorator.php
@@ -6,7 +6,12 @@ use Drupal\Core\Plugin\Discovery\ContainerDerivativeDiscoveryDecorator;
 
 class IntentDerivativeDiscoveryDecorator extends ContainerDerivativeDiscoveryDecorator {
 
+  /**
+   * {@inheritdoc}
+   */
   protected function encodePluginId($base_plugin_id, $derivative_id) {
+
+    // Use derivative_id (intent name) as plugin ID.
     if ($derivative_id) {
       return $derivative_id;
     }
